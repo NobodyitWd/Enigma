36b7df1c0ee59a5788aaa2386b016d7b0d10a23d
NobodyitWd <123703598+NobodyitWd@users.noreply.github.com>
2024-09-10 19:36:28 +0300

    Update Enigma.py

--- Enigma/Enigma.py
+++ Enigma/Enigma.py
@@ -1,4 +1,4 @@
-def Transform(ciphertext):
+def Transform(ciphertext): # Переобразуем числа в буквы.
     for i in range(len(ciphertext)):
         ciphertext[i] = ciphertext[i]%len(alpha)
         ciphertext[i] = alpha[ciphertext[i]]
@@ -10,6 +10,7 @@ originalMessage = ""
 
 print("The key must consist of 5 characters.")
 keyinp = input("Input key:    ")
+
 if len(keyinp) != 5:
     print("The key must consist of 5 characters.\n")
 
@@ -18,24 +19,25 @@ else:
     message = input("Input message:    ")
     message = message.upper()
     keyinp = keyinp.upper()
-    for i in keyinp:
+
+    for i in keyinp: # Если символа ключа нет в алфавите: завершить работу программы.
         if i in alpha:
             key.append(i)
         else:
             print("Uncorrect key.")
             exit(0)
 
-    for i in message:
+    for i in message: # Если символа сообщения нет в алфавите: удалить символ.
         if i not in alpha:
             message = message.replace(i,"")
 
-    for i in range(len(message)):
+    for i in range(len(message)): # Переобразуем ИЗНАЧАЛЬНОЕ сообщение из текста в числа.
         converter.append(alpha.index(message[i]))
 
 
-    ciphertext = ([0]*len(message))
+    ciphertext = ([0]*len(message)) # Шифро текст. Длинною в сообщение. Пока что из нулей.
     i = 0
-    for l in range(len(alpha)):
+    for l in range(len(alpha)): # Валы "Энигмы" (шифраторы). Принцип работы есть в иных источниках.
         keyl = alpha.index(key[4]) + l
         for k in range(len(alpha)):
             keyk = alpha.index(key[3]) + k
@@ -44,14 +46,12 @@ else:
                 for x in range(len(alpha)):
                     keyx = alpha.index(key[1]) + x
                     for y in range(len(alpha)):
-
                         if i != len(message):
-
                             oldkey = (alpha.index(key[0]) + y)
-                            keyy = (alpha.index(key[0]) + y + keyx + keyz + keyk + keyl) % len(alpha)
+                            keyy = (oldkey + keyx + keyz + keyk + keyl) % len(alpha) # V = (Прибавим положение каждого вала, и поделем по модулю длинны алфавита).
 
-                            ciphertext[i] = alpha.index(message[i]) + keyy
-                            originalMessage += alpha[(converter[i] - oldkey - keyx - keyz - keyk - keyl) % len(alpha)]
+                            ciphertext[i] = alpha.index(message[i]) + keyy # Текущее сообщение + V = шифротекст.
+                            originalMessage += alpha[(converter[i] - oldkey - keyx - keyz - keyk - keyl) % len(alpha)] # Все тоже самое но наоборот и с ИЗНАЧАЛЬНЫМ сообщением.
 
                             i += 1
                         else:
@@ -61,3 +61,5 @@ else:
 
     print("".join(Transform(ciphertext)))
     print(originalMessage)
+
+# В одном сообщении возможно зашифровать не более 6590815232 слов.
